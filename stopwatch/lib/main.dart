3e8ca0f71c4733806dba6ce373f3917c0ff5acf6
Ralph Lee <ralph.lee@live.ca>
2020-06-09 22:18:23 -0700

    Setup neuTimer widget

--- stopwatch/lib/main.dart
+++ stopwatch/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'widgets/neuStopwatch.dart';
+import 'widgets/neuTimer.dart';
 
 void main() {
   runApp(StopWatch());
@@ -48,7 +49,7 @@ class StopWatch extends StatelessWidget {
           ],
           color: Color(0xffdddddd),
         ),
-        body: NeuStopwatch(),
+        body: NeuTimer(),
       ),
     );
   }
