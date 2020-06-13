6ff52d496e69b15327144288e8b39662548de7f2
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-06-13 14:48:11 -0700

    Created a new widget for the control button set and replace the code

--- stopwatch/lib/widgets/neuStopwatch.dart
+++ stopwatch/lib/widgets/neuStopwatch.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-import 'package:provider/provider.dart';
-import 'package:stopwatch/widgets/stopwatch_ctr.dart';
+import 'package:stopwatch/widgets/stopwatchController.dart';
 import 'package:stopwatch/widgets/timeDisplayer.dart';
 
 class NeuStopwatch extends StatelessWidget {
@@ -11,66 +10,17 @@ class NeuStopwatch extends StatelessWidget {
       child: Column(
         children: <Widget>[
           SizedBox(
-            height: 30,
+            height: 100,
           ),
           TimeDisplayer(),
           SizedBox(
             height: 130,
           ),
-          Consumer<StopwatchListener>(
-            builder: (context, value, child) => Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  NeumorphicButton(
-                    child: Text(
-                      "Reset",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey,
-                          fontSize: 18),
-                    ),
-                    onPressed: ()=> value.resetStopwatch() ,
-                    style: NeumorphicStyle(
-                      shape: NeumorphicShape.concave,
-                      depth: 3,
-                    ),
-                    padding: EdgeInsets.only(
-                        left: 20, top: 15, right: 20, bottom: 15),
-                  ),
-                  NeumorphicButton(
-                    padding: EdgeInsets.all(10),
-                    style: NeumorphicStyle(
-                      boxShape: NeumorphicBoxShape.circle(),
-                      depth: 3,
-                    ),
-                    child: Icon(
-                      Icons.pause,
-                      color: Colors.grey,
-                      size: 40,
-                    ),
-                    onPressed: () => value.stopStopwatch(),
-                  ),
-                  NeumorphicButton(
-                    child: Text(
-                      "Start",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey,
-                          fontSize: 18),
-                    ),
-                    onPressed: ()=>value.startStopwatch(),
-                    style: NeumorphicStyle(
-                      shape: NeumorphicShape.concave,
-                      depth: 3,
-                    ),
-                    padding: EdgeInsets.only(
-                        left: 28, top: 15, right: 28, bottom: 15),
-                  ),
-                ]),
-          )
+          StopwatchController(),
         ],
       ),
     );
   }
 }
+
+
