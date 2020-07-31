8b6de2035293f1f0d1c4ff7f95e69dc38c3d504e
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-31 00:06:15 -0700

    Separate converToString function from start timer function

--- stopwatch/lib/tools/timer_ctr.dart
+++ stopwatch/lib/tools/timer_ctr.dart
@@ -8,13 +8,55 @@ class TimerListener with ChangeNotifier {
 
   bool checkTimer = true;
 
+  void convertToString() {
+    if (setTime < 60) {
+      var st = setTime.toString();
+      if (setTime < 10) st = "0" + st;
+
+      timetodisplay = "00:00:" + st;
+
+      notifyListeners();
+    } else if (setTime < 3600) {
+      int m = setTime ~/ 60;
+      int s = setTime - (60 * m);
+
+      var sm = m.toString();
+      var ss = s.toString();
+
+      if (m < 10) sm = "0" + sm;
+      if (s < 10) ss = "0" + ss;
+
+      timetodisplay = "00:" + sm + ":" + ss;
+
+      notifyListeners();
+    } else {
+      int h = setTime ~/ 3600;
+      int t = setTime - (3600 * h);
+      int m = t ~/ 60;
+      int s = t - (60 * m);
+
+      var sh = h.toString();
+      var sm = m.toString();
+      var ss = s.toString();
+
+      if (h < 10) sh = "0" + sh;
+      if (m < 10) sm = "0" + sm;
+      if (s < 10) ss = "0" + ss;
+
+      timetodisplay = sh + ":" + sm + ":" + ss;
+      notifyListeners();
+    }
+  }
+
   void incrementHour() {
     setTime = setTime + 3600;
+    convertToString();
     notifyListeners();
   }
 
   void incrementMinute() {
     setTime = setTime + 60;
+    convertToString();
     notifyListeners();
   }
 
@@ -27,45 +69,9 @@ class TimerListener with ChangeNotifier {
         if (setTime < 0 || checkTimer == false) {
           t.cancel();
           notifyListeners();
-        } else if (setTime < 60) {
-          var st = setTime.toString();
-          if (setTime < 10) st = "0" + st;
-
-          timetodisplay = "00:00:" + st;
-          setTime = setTime - 1;
-
-          notifyListeners();
-        } else if (setTime < 3600) {
-          int m = setTime ~/ 60;
-          int s = setTime - (60 * m);
-
-          var sm = m.toString();
-          var ss = s.toString();
-
-          if (m < 10) sm = "0" + sm;
-          if (s < 10) ss = "0" + ss;
-
-          timetodisplay = "00:" + sm + ":" + ss;
-          setTime = setTime - 1;
-
-          notifyListeners();
         } else {
-          int h = setTime ~/ 3600;
-          int t = setTime - (3600 * h);
-          int m = t ~/ 60;
-          int s = t - (60 * m);
-
-          var sh = h.toString();
-          var sm = m.toString();
-          var ss = s.toString();
-
-          if (h < 10) sh = "0" + sh;
-          if (m < 10) sm = "0" + sm;
-          if (s < 10) ss = "0" + ss;
-
-          timetodisplay = sh + ":" + sm + ":" + ss;
-          setTime = setTime - 1;
-          notifyListeners();
+          setTime -= 1;
+          convertToString();
         }
       },
     );
